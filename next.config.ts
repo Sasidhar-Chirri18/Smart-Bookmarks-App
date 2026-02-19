1cb8b1de5a12bc1e26519fc20a627553dde3d368
Sasidhar-Chirri18 <sasidharchirri511@gmail.com>
2026-02-19 16:52:43 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   devIndicators: {
+//     buildActivity: false,
+//     buildActivityPosition: 'bottom-right',
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: {
-    buildActivity: false,
-    buildActivityPosition: 'bottom-right',
+    position: "bottom-right",
   },
 };
 
